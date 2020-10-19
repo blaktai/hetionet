d19c096ff27ff486f81ef26ad1073691653ff67f
blaktai <55172595+blaktai@users.noreply.github.com>
2020-10-19 13:10:15 -0400

    Feature/graph parser (#4)
    * ADD hetionet graph data
    
    * ADD gitignore to keep repo clean
    
    * ADD hetio net utilities for reading and parsing the graph
    
    * UPDATE the command line tool to build sub graphs of hetionet
    
    * ADD entry point file

---
+++ cli.py
@@ -0,0 +1,3 @@
+from cli.hetionet import cli
+if __name__ == '__main__':
+    cli()
--- cli/hetionet.py
+++ cli/hetionet.py
@@ -1,14 +1,20 @@
+import pprint
+
 import click 
+from utils.reader import read_text
+from utils.graph_parser import parse_nodes, build_subgraphs    
+
 @click.command()
-@click.argument("file", type=click.File("r"))
-def cli(file):
-    """This script prints the contents of a file.
+@click.argument("node_file", type=click.File("r"))
+@click.argument('edge_file', type=click.File("r"))
+def cli(node_file, edge_file):
+    """This script prints the sub graphs of a hetionet.
     \b
-    Write file to stdout:
-        hetionet foo.txt 
+    Print hetionet sub graphs to stdout:
+        hetionet nodes.tsv edges.tsv
     """
-    contents = file.read()
-    print(contents)
-    
-if __name__ == '__main__':
-    cli()
+    node_list = read_text(node_file, delimiter='\t', skip_header=True)
+    nodes = parse_nodes(node_list)
+    edges = read_text(edge_file, delimiter='\t', skip_header=True)
+    for graph in build_subgraphs(nodes, edges):
+        pprint.pprint(graph)
