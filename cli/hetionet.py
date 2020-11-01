e2077ece37ba695364bd538788df370d90045cf7
blaktai <55172595+blaktai@users.noreply.github.com>
2020-11-01 01:16:29 -0400

    FIX broken link and missing dependencies in neo4j storage file for cli

--- cli/hetionet.py
+++ cli/hetionet.py
@@ -3,7 +3,7 @@ import pprint
 import click 
 from utils.reader import read_text
 from utils.graph_parser import parse_nodes, build_subgraphs    
-# from db.neo4j.neo4j import create_hetionet
+from db.neo4j.neo4j import create_hetionet
 from db.redis.redis import create_redis_store, find_disease
 @click.command()
 @click.argument("node_file_path", type=click.Path(exists=True), required=True)
--- utils/reader.py
+++ utils/reader.py
@@ -37,3 +37,34 @@ def filter_edges(df):
     del df['metaedge']
     df = df.dropna()
     return df
+
+def load(filename, delimiter = '\t'):
+    return pd.read_csv(filename, delimiter=delimiter)
+
+def clean_up(node_df, edge_df, edge_types, cols = ['Anatomy', 'Compound', 'Disease', 'Gene']):
+    node_df = clean_up_nodes(node_df, cols)
+    edge_df = clean_up_edges(edge_df, cols, edge_types)
+    node_cols = node_df.columns
+    edge_cols = edge_df.columns
+    df = edge_df.merge(node_df, how='inner', left_on='source', right_on='id')
+    edge_df = df[edge_cols]
+    return df, node_df, edge_df
+
+def clean_up_edges(edge_df, cols, edge_types):
+    regex = re.compile('(.+)::.+')
+    edge_cols = edge_df.columns
+    edge_df['target_kind'] = edge_df.target.str.replace(regex, r'\1')
+    f = (edge_df.target_kind.isin(cols) & edge_df.metaedge.isin(edge_types))
+    subset_edges_df = edge_df[edge_df.target_kind.isin(cols)]
+    subset_edges_df = subset_edges_df[subset_edges_df.metaedge.isin(edge_types)]
+    return subset_edges_df[edge_cols]
+
+def clean_up_nodes(node_df, cols):
+    return node_df[node_df.kind.isin(cols)]
+
+def load_dataframes(node_file_path, edge_file_path, delimiter='\t',
+    edge_types = ['AuG', 'AdG', 'CrC', 'CtD', 'CpD', 'CuG', 'CdG', 'DrD', 'DlA'],
+    node_types = ['Anatomy', 'Compound', 'Disease', 'Gene']):
+    nodes_df = load(node_file_path, delimiter=delimiter)
+    edges_df = load(edge_file_path, delimiter=delimiter)
+    return clean_up(nodes_df, edges_df, edge_types, node_types)
