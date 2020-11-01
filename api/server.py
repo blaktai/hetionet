30c16dbc14ea45bf8a2ca2bac78d45008e1c092d
blaktai <55172595+blaktai@users.noreply.github.com>
2020-11-01 06:05:31 -0500

    UPDATE integrated the api server with db code

--- api/server.py
+++ api/server.py
@@ -4,6 +4,8 @@ import os
 from flask import Flask, render_template, request
 from flask_restx import Resource, Api
 from utils.reader import read_text, read_text_from_disk
+from db.redis.redis import find_disease
+from db.neo4j.neo4j import find_disease_treatments
 
 LOG = logging.getLogger(__name__)
 FILENAME = "hetionet-v1.0-nodes.tsv"
@@ -13,12 +15,14 @@ api = Api(app)
 @api.route('/v1/diseases/<string:id>')
 class Disease(Resource):
     def post(self, id):
-        return id
+        result = find_disease(id)
+        return result if result is not None else {}
 
-@api.route('/v1/treatments/<string:disease_id>')
+@api.route('/v1/treatments/<string:disease_name>')
 class Treatment(Resource):
-    def post(self, disease_id):
-        return disease_id
+    def post(self, disease_name):
+        result = find_disease_treatments(disease_name)
+        return result if result is not None else []
 
 
 @app.route('/home')
