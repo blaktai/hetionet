cb3451f671a20c2f27730e6618f778ce0ad151ce
blaktai <55172595+blaktai@users.noreply.github.com>
2020-11-01 00:09:24 -0400

    UPDATE add neo4j storage to cli

--- cli/hetionet.py
+++ cli/hetionet.py
@@ -3,19 +3,18 @@ import pprint
 import click 
 from utils.reader import read_text
 from utils.graph_parser import parse_nodes, build_subgraphs    
+from db.neo4j.hetionet_pandas import create_hetionet
 
 @click.command()
-@click.argument("node_file", type=click.File("r"), required=True)
-@click.argument('edge_file', type=click.File("r"), required=True)
+@click.argument("node_file_path", type=click.Path(exists=True), required=True)
+@click.argument('edge_file_path', type=click.Path(exists=True), required=True)
 @click.option("--delimiter", default="\t", help="Delimiter of file format")
-def cli(node_file, edge_file, delimiter):
+# @click.parameter()
+def cli(node_file_path, edge_file_path, delimiter):
     """This script prints the sub graphs of a hetionet.
     \b
     Print hetionet sub graphs to stdout:
         hetionet nodes.tsv edges.tsv
     """
-    node_list = read_text(node_file, delimiter=delimiter, skip_header=True)
-    nodes = parse_nodes(node_list)
-    edges = read_text(edge_file, delimiter=delimiter, skip_header=True)
-    for graph in build_subgraphs(nodes, edges):
-        pprint.pprint(graph)
+    print(node_file_path, edge_file_path, delimiter)
+    create_hetionet(node_file_path, edge_file_path, delimiter)
